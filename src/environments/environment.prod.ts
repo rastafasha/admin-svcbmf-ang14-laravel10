164908f4a8c4c719a37ee1901a3c31a849f547d4
Malcolm Cordova <41023438+rastafasha@users.noreply.github.com>
2025-05-23 20:40:07 -0400

    en produccion

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,10 +1,10 @@
 export const environment = {
   production: true,
   //apirest local
-  // apiUrl: "http://127.0.0.1:8000/api",
-  // apiUrlMedia: "http://127.0.0.1:8000/storage/app/",
+  apiUrl: "http://127.0.0.1:8000/api",
+  apiUrlMedia: "http://127.0.0.1:8000/storage/app/",
   //remoto
-  apiUrl: "https://svcbmf.com/backend-api-svcbmf/public/api",
-  apiUrlMedia: "https://svcbmf.com/backend-api-svcbmf/storage/app/",
+  // apiUrl: "https://www.svcbmf.com/backend-api-svcbmf/public/api",
+  // apiUrlMedia: "https://www.svcbmf.com/backend-api-svcbmf/storage/app/",
 
 };
